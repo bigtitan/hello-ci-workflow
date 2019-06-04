115b15d8baf0f7fe43724003cdb1830c1be72bff
titan <titan.han@gmail.com>
2019-06-04 17:45:01 +0800

    init git

---
+++ index.js
@@ -0,0 +1,13 @@
+// index.js
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('<b>Hello World!</b>');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
---
+++ test/test.js
@@ -0,0 +1,17 @@
+// test1/test.js
+var assert = require("assert")
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(0));
+    })
+  })
+})
+
+describe('Array', function(){
+  describe('#indexOf()', function(){
+    it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(0));
+    })
+  })
+})
